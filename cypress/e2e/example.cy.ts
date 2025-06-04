0a3833191160c6e9a64923328b2d6c1f760786cf
redondo <riad.reda.fethi@gmail.com>
2025-06-04 12:40:28 +0200

    supression cypress

--- cypress/e2e/example.cy.ts
+++
@@ -1,7 +0,0 @@
-// cypress/e2e/example.cy.ts
-describe('My First E2E Test', () => {
-  it('should visit the homepage', () => {
-    cy.visit('/');
-    cy.contains('Bienvenue'); // Change 'Bienvenue' par un texte que tu as sur ta page d'accueil
-  });
-});
